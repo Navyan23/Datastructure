6fd675bb8afed76be816e5094ae5b34101240b89
Navyan23 <97940058+Navyan23@users.noreply.github.com>
2022-02-21 12:45:00 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	   int a,b,sum;
+	   
+	   cout<<"Enter the  two integer\n";
+	   cin>>a>>b;
+	   sum=a+b;
+	   cout<<"\n sum is="<<sum;
+	   getch();
+	   
+}
